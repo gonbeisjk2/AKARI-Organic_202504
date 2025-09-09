3bc7051ea0a5a03029966fb7a1b5e730c5d87e79
taro shinjyuku <tarosjk@gmail.com>
2025-09-09 14:42:55 +0900

    FAQのスタイリングと動きの実装完了

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// グローバルナビゲーション
 const navOpen = document.querySelector('.l-header__navBtn');
 const navClose = document.querySelector('.l-header__navClose');
 
@@ -7,4 +8,11 @@ navOpen.addEventListener('click', () => {
 
 navClose.addEventListener('click', () => {
   document.body.classList.remove('is-nav-open');
+});
+
+// FAQ (jQuery)
+$('.p-faq__listQ button').on('click', function (e) {
+  // e.stopPropagation(); //親要素へのクリックの影響を阻止する（バブリングの抑止）
+  $(this).closest('.p-faq__listItem').find('.p-faq__listA').slideToggle();
+  $(this).toggleClass('is-open');
 });
